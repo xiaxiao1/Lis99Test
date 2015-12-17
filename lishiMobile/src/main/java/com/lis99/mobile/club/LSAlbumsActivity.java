09e6acebf726afd2c970e0c75759e0510d3ca20e
YY <yangyang@lis99.com>
2015-12-17 10:18:27 +0800

    3.9.1 合并增加的

---
+++ lishiMobile/src/main/java/com/lis99/mobile/club/LSAlbumsActivity.java
@@ -0,0 +1,19 @@
+package com.lis99.mobile.club;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lis99.mobile.R;
+
+public class LSAlbumsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lsalbums);
+    }
+
+
+
+
+}
