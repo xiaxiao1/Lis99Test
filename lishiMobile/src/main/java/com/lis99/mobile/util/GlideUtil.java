3f19660a77f995ce93cbb30891753200142ad004
YY <yangyang@lis99.com>
2016-07-26 18:00:36 +0800

    替换图片

---
+++ lishiMobile/src/main/java/com/lis99/mobile/util/GlideUtil.java
@@ -0,0 +1,44 @@
+package com.lis99.mobile.util;
+
+import android.app.Activity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.lis99.mobile.R;
+
+/**
+ * Created by yy on 16/7/26.
+ */
+public class GlideUtil {
+
+    private static GlideUtil instance;
+
+    public static GlideUtil getInstance ()
+    {
+        if ( instance == null ) instance = new GlideUtil();
+        return instance;
+    }
+
+    /**
+     *          用户头像
+     * @param activity
+     * @param url
+     * @param img
+     */
+    public void getHeadImageView (Activity activity, String url, ImageView img )
+    {
+        Glide.with(activity)
+                .load(url)
+                .placeholder(R.drawable.ls_nologin_header_icon)
+                .error(R.drawable.ls_nologin_header_icon)
+                .fallback(R.drawable.ls_nologin_header_icon)
+                .crossFade()
+                .into(img);
+    }
+
+
+
+
+
+
+}
