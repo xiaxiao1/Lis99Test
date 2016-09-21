7d9bd63f96c76171f35d175a7514be0ec07f7f7e
YY <yangyang@lis99.com>
2016-09-21 10:43:12 +0800

    客服添加

---
+++ lishiMobile/src/main/java/com/lis99/mobile/kf/easemob/KFCommon.java
@@ -0,0 +1,7 @@
+package com.lis99.mobile.kf.easemob;
+
+/**
+ * Created by yy on 16/9/18.
+ */
+public class KFCommon {
+}
---
+++ lishiMobile/src/main/java/com/lis99/mobile/kf/easemob/helpdesk/interfaces/IListener.java
@@ -0,0 +1,8 @@
+package com.lis99.mobile.kf.easemob.helpdesk.interfaces;
+
+/**
+ * 发送广播接口,用于通知
+ */
+public interface IListener {
+    void notifyEvent(String eventStr, Object eventObj);
+}
