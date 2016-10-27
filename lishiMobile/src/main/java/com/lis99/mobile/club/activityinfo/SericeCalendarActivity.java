2128872be5c50b89b0efa53398fcf74c26d4b6ba
YY <yangyang@lis99.com>
2016-10-27 12:05:49 +0800

    修改日历的一个选择范围的一个BUG

--- lishiMobile/src/main/java/com/lis99/mobile/club/activityinfo/SericeCalendarActivity.java
+++ lishiMobile/src/main/java/com/lis99/mobile/club/activityinfo/SericeCalendarActivity.java
@@ -364,7 +364,7 @@ public class SericeCalendarActivity extends LSBaseActivity {
     private CalendarInfo getLastDay ( CalendarInfo info, CalendarInfo last )
     {
         if ( last == null ) return info;
-        if ( info.year > last.year || info.month > last.month )
+        if ( info.year > last.year || (info.year == last.year && info.month > last.month) )
         {
             return info;
         }
@@ -380,7 +380,7 @@ public class SericeCalendarActivity extends LSBaseActivity {
     private CalendarInfo getFirstDay ( CalendarInfo info, CalendarInfo first )
     {
         if ( first == null ) return info;
-        if ( info.year < first.year || info.month < first.month )
+        if ( info.year < first.year || (info.year == first.year && info.month < first.month) )
         {
             return info;
         }
